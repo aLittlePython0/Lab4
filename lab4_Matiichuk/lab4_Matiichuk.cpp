72736f5802b8e9cc10801e6b1c867ac61877e067
Yaroslav Matiichuk <9653581@gmail.com>
2022-09-14 21:09:47 +0300

    sgdsgsdf

--- lab4_Matiichuk/lab4_Matiichuk.cpp
+++ lab4_Matiichuk/lab4_Matiichuk.cpp
@@ -1,10 +1,48 @@
-﻿// rerewet  
+﻿#include <iostream>
+#include <conio.h>
+#include <math.h>
 
-#include <iostream>
+using namespace std;
+
+class ClassArray
+{
+    friend int MainF(ClassArray);
+private:
+    int nArray = 0;
+public:
+
+
+    ClassArray(int nArray)
+    {
+        this->nArray = nArray;
+        int *ptrArray = new int[nArray];
+        //delete[]ptrArray;
+    }
+    
+    ~ClassArray()
+    {
+        delete ptrArray;
+    }
+
+    void MainF(ClassArray ppp)
+    {
+        for (int i = 0; i < nArray; i++) 
+        {
+            if (i % 2 == 0) 
+            {
+                ppp.ptrArray[i] = i - 5;
+                ppp++;
+            }
+            else ppp.ptrArray[i] = i;
+        }
+        cout << ppp;
+    }
+
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ClassArray first(11);
+    return 0;
 }
-
-//o
